b15135e896df1e40bae1caaba352cba56e267ec7
issverg <issverg@gmail.com>
2025-08-28 13:20:24 +0300

    adding utils, constants, update apostille page

--- src/app/(routes)/translate_docs_apostille/page.tsx
+++ src/app/(routes)/translate_docs_apostille/page.tsx
@@ -2,40 +2,21 @@ import BackButton from "@/app/components/commons/BackButton";
 import styles from "./page.module.css";
 import TopPageContent from "@/app/components/commons/TopPageContent";
 import DoubleColumn from "@/app/components/commons/DoubleColumn";
+import trans_docs_apostille from "../../utils/Constants";
 
 const TranslateDocsApostille = () => {
-    const article = `   Мы предлагаем вам переводы документов любой сложности практически на любые языки.
-                        По просьбе клиентов мы можем сделать переводы в течение одного дня.
-                        У нас есть обширная база переводчиков, имеющих большой опыт переводов по различной тематике.`;
-    const article_one = [
-        "Технические документы", 
-        "Медицинские документы", 
-        "Экономическую документацию", 
-        "Личные и стандартные документы", 
-        "на иврит, русский язык, украинский, немецкий, испанский, французский, чешский, литовский, болгарский, румынский и, конечно, на английский язык.", 
-        "Также переводим документы с иврита на различные языки.",
-        "У нас вы можете заказать срочные переводы."
-    ];
-    const article_two = [
-        "Переводы документов для МВД", 
-        "Переводы для приглашения мужа/ жены", 
-        "Переводы для оформления гражданства мужа/ жены", 
-        "Переводы документов для получения гражданства одинокого родителя", 
-        "Переводы для получения гражданства одинокого родителя израильского солдата", 
-        "Переводы для оформления репатриации туристам, находящимся в Израиле", 
-        "И многое другое"];
     return (
         <div className={styles.tda_wrapper}>
             <BackButton />
             <TopPageContent 
-                title="ПЕРЕВОД ДОКУМЕНТОВ АПОСТИЛЬ" 
-                article={article} 
-                img_src="/images/translate_docs_apostille.png" />
+                title={trans_docs_apostille.title} 
+                article={trans_docs_apostille.article} 
+                img_src={trans_docs_apostille.img_src} />
             <DoubleColumn 
-                header_one="Мы поможем вам перевести:" 
-                article_one={article_one} 
-                header_two="Мы предлагаем готовые пакеты переводов, например:" 
-                article_two={article_two} />
+                header_one={trans_docs_apostille.header_one} 
+                article_one={trans_docs_apostille.article_one}
+                header_two={trans_docs_apostille.header_two} 
+                article_two={trans_docs_apostille.article_two} />
         </div>
     );
 }
